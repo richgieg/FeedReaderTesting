497e43f14fa6f0db7094c9b0c1c800c8de093101
Richard Gieg <richgieg@users.noreply.github.com>
2016-01-24 16:22:21 -0800

    Add test suite for initial entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -79,6 +79,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -93,4 +94,6 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+    });
 }());
